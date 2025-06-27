57baebb364a7440b89119c5889a1e7dbbec01a6c
Daiki Tamada <dtamada@wisc.edu>
2025-06-27 11:22:06 -0500

    update

--- setup.py
+++ setup.py
@@ -7,27 +7,30 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    version="0.1.0", # `version` is now defined here
-    description="A package for CSEMRI with C++ accelerated components.", # `description` is here
-    long_description=long_description, # `readme` content is here
+    # THIS IS THE CRITICAL FIX:
+    # We must explicitly define the package name here.
+    name="PyCSEMRI",
+
+    version="0.1.0",
+    description="A package for CSEMRI with C++ accelerated components.",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     
-    # These were not declared as dynamic, so they stay here
     author="Daiki Tamada",
     author_email="dtamada@wisc.edu",
     url="https://github.com/your-username/PyCSEMRI_clean",
     license="GPL",
     
     packages=["package"],
+    # This line is no longer needed with scikit-build and a flat structure
+    # package_dir={"": "package"}, 
     cmake_source_dir=".",
     
-    # These dependencies correspond to the `dynamic` "dependencies" field
     install_requires=[
         "numpy<1.20; python_version == '3.6'",
         "numpy; python_version >= '3.7'",
     ],
     
-    # This corresponds to the `dynamic` "requires-python" field
     python_requires=">=3.6",
 )
 
