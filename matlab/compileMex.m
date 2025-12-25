9fa34d3a1c4c3f30b7ab9c76df36450f83b74c2b
Daiki Tamada <dtamada@wisc.edu>
2025-12-25 17:07:39 -0600

    Add static libraries for MATLAB MEX

--- matlab/compileMex.m
+++ matlab/compileMex.m
@@ -1,5 +1,5 @@
-mex -I../libuwwfs/ -L../libuwwfs/ -luwwfs fwFit_VarproLS_1r2star_mex.cpp 
-mex -I../libuwwfs/ -L../libuwwfs/ -luwwfs VARPRO_LUT_mex.cpp 
-mex -I../libuwwfs/ -L../libuwwfs/ -luwwfs fwFit_ComplexLS_1r2star_mex.cpp 
-mex -I../libuwwfs/ -L../libuwwfs/ -luwwfs fwFit_MixedLS_1r2star_mex.cpp 
-mex -I../libuwwfs/ -L../libuwwfs/ -luwwfs fwFit_MagnLS_1r2star_mex.cpp 
+mex -I../libuwwfs/ -L../libuwwfs/ fwFit_VarproLS_1r2star_mex.cpp ../libuwwfs/libuwwfs.a
+mex -I../libuwwfs/ -L../libuwwfs/ VARPRO_LUT_mex.cpp  ../libuwwfs/libuwwfs.a
+mex -I../libuwwfs/ -L../libuwwfs/ fwFit_ComplexLS_1r2star_mex.cpp ../libuwwfs/libuwwfs.a
+mex -I../libuwwfs/ -L../libuwwfs/ fwFit_MixedLS_1r2star_mex.cpp ../libuwwfs/libuwwfs.a
+mex -I../libuwwfs/ -L../libuwwfs/ fwFit_MagnLS_1r2star_mex.cpp ../libuwwfs/libuwwfs.a
